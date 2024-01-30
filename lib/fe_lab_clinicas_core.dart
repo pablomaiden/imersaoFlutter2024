702f21ae66190d264d97e31f47dc4dded230dab5
Pablo Roberto da Silva Guimarães <pablomaiden@gmail.com>
2024-01-30 13:17:10 -0300

    Enviando a construção do Rest Cliente e dos Interceptors

--- lib/fe_lab_clinicas_core.dart
+++ lib/fe_lab_clinicas_core.dart
@@ -4,3 +4,4 @@ export 'src/fp/either.dart';
 export 'src/helpers/messages.dart';
 export 'src/nil.dart';
 export 'src/unit.dart';
+export 'src/restClient/rest_client.dart';
---
+++ lib/src/constants/local_constants_storage.dart
@@ -0,0 +1,5 @@
+
+
+sealed class LocalConstantsStorage{
+  static const accesToken='ACCESS_TOKEN_KEY';
+}
