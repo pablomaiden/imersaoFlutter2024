3d6fc593c5370a0599ff63d9e21e1a6347057d86
2° Ten Pablo <guimaraes.pablo@CDS.LOCAL>
2024-01-31 10:53:10 -0300

    LabClinicasCorConfig

--- lib/fe_lab_clinicas_core.dart
+++ lib/fe_lab_clinicas_core.dart
@@ -3,5 +3,5 @@ library fe_lab_clinicas_core;
 export 'src/fp/either.dart';
 export 'src/helpers/messages.dart';
 export 'src/nil.dart';
-export 'src/unit.dart';
 export 'src/restClient/rest_client.dart';
+export 'src/unit.dart';
