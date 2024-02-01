57265baed3565242338d2a67506cc4cd25bdbc38
2° Ten Pablo <guimaraes.pablo@CDS.LOCAL>
2024-02-01 09:29:27 -0300

    Subindo o rest client

--- lib/src/restClient/rest_client.dart
+++ lib/src/restClient/rest_client.dart
@@ -1,5 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'package:fe_lab_clinicas_core/src/restClient/interceptors/auth_interceptors.dart';
 
+final class RestClient extends DioForNative {
+  RestClient(String baseUrl)
+      : super(BaseOptions(
+            baseUrl: baseUrl,
+            connectTimeout: Duration(seconds: 10),
+            receiveTimeout: Duration(seconds: 60))) {
+    interceptors.addAll([
+      LogInterceptor(requestBody: true, responseBody: true),
+      AuthInterceptors()
+    ]);
+  }
 
-class RestClient{
-  
+  RestClient get auth {
+    options.extra['DIO_AUTH_KEY'] = true;
+    return this;
+  }
+
+  RestClient get unAuth {
+    options.extra['DIO_AUTH_KEY'] = false;
+    return this;
+  }
 }
